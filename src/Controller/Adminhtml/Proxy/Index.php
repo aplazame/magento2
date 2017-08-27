7228ee5b9f99564bdc44d95c9dacb3847a0e3a3b
Maks3w <github.maks3w@virtualplanets.net>
2017-08-27 20:25:40 +0200

    [admin] Proxy

---
+++ src/Controller/Adminhtml/Proxy/Index.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace Aplazame\Payment\Controller\Adminhtml\Proxy;
+
+use Aplazame\Payment\Model\Api\AplazameClient;
+use Magento\Framework\App\Action\Action;
+use Magento\Framework\App\Action\Context;
+
+class Index extends Action
+{
+    /**
+     * @var AplazameClient
+     */
+    private $aplazameClient;
+
+    public function __construct(
+        Context $context,
+        AplazameClient $aplazameClient
+    ) {
+        $this->aplazameClient = $aplazameClient;
+
+        parent::__construct($context);
+    }
+
+    public function execute()
+    {
+        $request = $this->getRequest();
+
+        $data = json_decode($request->getParam('data'));
+
+        $response = $this->aplazameClient->apiClient->request(
+            $request->getParam('method'),
+            $request->getParam('path'),
+            $data
+        );
+
+        $this->getResponse()
+             ->setHeader('Content-Type', 'application/json')
+             ->setBody(json_encode($response))
+        ;
+    }
+}
