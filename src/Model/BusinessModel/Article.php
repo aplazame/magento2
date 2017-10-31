20d059bb1a4805ca32a6587fc96d97fddff79128
Maks3w <github.maks3w@virtualplanets.net>
2017-10-31 13:41:41 +0100

    Add image url to checkout

--- src/Model/BusinessModel/Article.php
+++ src/Model/BusinessModel/Article.php
@@ -17,12 +17,21 @@ class Article
         $aArticle->id = $product->getId();
         $aArticle->name = $item->getName();
         $aArticle->url = $product->getProductUrl();
-        $aArticle->image_url = $product->getImageUrl();
         $aArticle->quantity = intval($item->getQtyOrdered());
         $aArticle->price = Decimal::fromFloat($item->getPrice() + $discounts);
         $aArticle->description = substr($product->getDescription(), 0, 255);
         $aArticle->tax_rate = Decimal::fromFloat($item->getTaxPercent());
         $aArticle->discount = Decimal::fromFloat($item->getDiscountAmount());
+
+        $objectManager = \Magento\Framework\App\ObjectManager::getInstance();
+        /** @var \Magento\Catalog\Block\Product\ImageBuilder $imageBuilder */
+        $imageBuilder = $objectManager->get('Magento\Catalog\Block\Product\ImageBuilder');
+        $image = $imageBuilder->setProduct($product)
+            ->setImageId('category_page_list')
+            ->create()
+        ;
+
+        $aArticle->image_url = $image->getImageUrl();
 
         return $aArticle;
     }
