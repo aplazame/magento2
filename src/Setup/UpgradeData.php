44889289cc972e9f341fb7864a3867c8677940ca
Maks3w <github.maks3w@virtualplanets.net>
2017-11-14 18:00:28 +0100

    Migration script

---
+++ src/Setup/UpgradeData.php
@@ -0,0 +1,79 @@
+<?php
+
+namespace Aplazame\Payment\Setup;
+
+use Aplazame\Api\Client;
+use Magento\Framework\Setup\ModuleContextInterface;
+use Magento\Framework\Setup\ModuleDataSetupInterface;
+use Magento\Framework\Setup\UpgradeDataInterface;
+
+class UpgradeData implements UpgradeDataInterface
+{
+    /**
+     * @var \Aplazame\Payment\Gateway\Config\Config
+     */
+    private $aplazameConfig;
+
+    /**
+     * @var \Magento\Framework\Url
+     */
+    private $urlBuilder;
+
+    /**
+     * @var \Magento\Framework\App\Config\ValueFactory
+     */
+    private $configValueFactory;
+
+    public function __construct(
+        \Aplazame\Payment\Gateway\Config\Config $aplazameConfig,
+        \Magento\Framework\Url $urlBuilder,
+        \Magento\Framework\App\Config\ValueFactory $configValueFactory
+    ) {
+        $this->aplazameConfig = $aplazameConfig;
+        $this->urlBuilder = $urlBuilder;
+        $this->configValueFactory = $configValueFactory;
+    }
+
+    public function upgrade(ModuleDataSetupInterface $setup, ModuleContextInterface $context)
+    {
+        if (!empty($this->aplazameConfig->getPrivateApiKey())) {
+            $response = $this->setupConfirmationUrl();
+
+            $this->setPublicKey($response['public_api_key']);
+        }
+    }
+
+    private function setupConfirmationUrl()
+    {
+        $apiClient = new Client(
+            $this->aplazameConfig->getApiBaseUri(),
+            ($this->aplazameConfig->isSandbox() ? Client::ENVIRONMENT_SANDBOX : Client::ENVIRONMENT_PRODUCTION),
+            $this->aplazameConfig->getPrivateApiKey()
+        );
+
+        $response = $apiClient->patch('/me', [
+            'confirmation_url' => $this->urlBuilder->getUrl(
+                'aplazame/api/index',
+                [
+                    '_query' => [
+                        'path' => '/confirm/',
+                    ],
+                    '_nosid' => true,
+                ]
+            ),
+        ]);
+
+        return $response;
+    }
+
+    private function setPublicKey($publicKey)
+    {
+        $path = 'payment/aplazame_payment/public_api_key';
+        $this->configValueFactory->create()
+             ->load($path, 'path')
+             ->setValue($publicKey)
+             ->setPath($path)
+             ->save()
+        ;
+    }
+}
