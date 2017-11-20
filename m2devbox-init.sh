77f8e2130fabe6aa761d26cbfb79f00446aeff8b
Maks3w <github.maks3w@virtualplanets.net>
2017-11-20 13:08:59 +0300

    Docker

--- m2devbox-init.sh
+++ m2devbox-init.sh
@@ -9,9 +9,9 @@ echo 'Build docker images'
 docker-compose up --build -d
 
 echo 'Install Magento'
-docker-compose exec --user magento2 web /home/magento2/scripts/m2init magento:install --no-interaction --magento-host=magento2.aplazame
-docker-compose exec --user magento2 web mkdir -p /var/www/magento2/app/code/Aplazame
-docker-compose exec --user magento2 web ln -s /aplazame /var/www/magento2/app/code/Aplazame/Payment
+docker-compose exec --user magento2 web /home/magento2/scripts/m2init magento:install --no-interaction --magento-host=magento2.aplazame && \
+docker-compose exec --user magento2 web mkdir -p /var/www/magento2/app/code/Aplazame && \
+docker-compose exec --user magento2 web ln -s /aplazame /var/www/magento2/app/code/Aplazame/Payment && \
 docker-compose exec --user magento2 web ln -s /aplazame /var/www/magento2/aplazame && \
 docker-compose exec --user magento2 web composer -d=/var/www/magento2/ require aplazame/aplazame-api-sdk && \
 docker-compose exec --user magento2 web php /var/www/magento2/bin/magento module:enable Aplazame_Payment && \
