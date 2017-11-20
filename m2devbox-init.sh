caf899f64d63a63ffc9e7698870a96e81c401e27
Maks3w <github.maks3w@virtualplanets.net>
2017-11-20 13:08:40 +0300

    Docker

--- m2devbox-init.sh
+++ m2devbox-init.sh
@@ -9,12 +9,12 @@ echo 'Build docker images'
 docker-compose up --build -d
 
 echo 'Install Magento'
-docker-compose exec -d --user magento2 web /home/magento2/scripts/m2init magento:install --no-interaction --magento-host=magento2.aplazame
-docker-compose exec -d --user magento2 web mkdir -p /var/www/magento2/app/code/Aplazame
-docker-compose exec -d --user magento2 web ln -s /aplazame /var/www/magento2/app/code/Aplazame/Payment
-docker-compose exec -d --user magento2 web ln -s /aplazame /var/www/magento2/aplazame
-docker-compose exec -d --user magento2 web composer -d=/var/www/magento2/ require aplazame/aplazame-api-sdk
-docker-compose exec -d --user magento2 web php bin/magento module:enable Aplazame_Payment
-docker-compose exec -d --user magento2 web php bin/magento setup:upgrade
-docker-compose exec -d --user magento2 web php bin/magento setup:di:compile
-docker-compose exec -d --user magento2 web php bin/magento cache:flush
+docker-compose exec --user magento2 web /home/magento2/scripts/m2init magento:install --no-interaction --magento-host=magento2.aplazame
+docker-compose exec --user magento2 web mkdir -p /var/www/magento2/app/code/Aplazame
+docker-compose exec --user magento2 web ln -s /aplazame /var/www/magento2/app/code/Aplazame/Payment
+docker-compose exec --user magento2 web ln -s /aplazame /var/www/magento2/aplazame && \
+docker-compose exec --user magento2 web composer -d=/var/www/magento2/ require aplazame/aplazame-api-sdk && \
+docker-compose exec --user magento2 web php /var/www/magento2/bin/magento module:enable Aplazame_Payment && \
+docker-compose exec --user magento2 web php /var/www/magento2/bin/magento setup:upgrade && \
+docker-compose exec --user magento2 web php /var/www/magento2/bin/magento setup:di:compile && \
+docker-compose exec --user magento2 web php /var/www/magento2/bin/magento cache:flush
