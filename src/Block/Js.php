e71660c4c7eaba6fe087ee571873a64fbdbf0f7a
Gonzalo Hidalgo Madrid <gonzalo@powerlinedesign.es>
2024-03-19 23:45:13 +0100

    Hide widget in selected language.

--- src/Block/Js.php
+++ src/Block/Js.php
@@ -3,8 +3,10 @@
 namespace Aplazame\Payment\Block;
 
 use Aplazame\Payment\Gateway\Config\Config;
+use Magento\Framework\App\Config\ScopeConfigInterface;
 use Magento\Framework\View\Element\Template;
 use Magento\Framework\View\Element\Template\Context;
+use Magento\Store\Model\ScopeInterface;
 
 class Js extends Template
 {
@@ -13,14 +15,20 @@ class Js extends Template
      */
     private $config;
 
+    /**
+     * @var ScopeConfigInterface
+     */
+    private $scopeConfig;
+
     public function __construct(
         Context $context,
         Config $config,
+        ScopeConfigInterface $scopeConfig,
         array $data = []
     ) {
         parent::__construct($context, $data);
-
         $this->config = $config;
+        $this->scopeConfig = $scopeConfig;
     }
 
     /**
@@ -33,10 +41,22 @@ class Js extends Template
 
     protected function _toHtml()
     {
-        if (!$this->config->isActive()) {
+        if (!$this->config->isActive() || $this->isHiddenInCurrentLanguage()) {
             return '';
         }
 
         return parent::_toHtml();
+    }
+
+    public function isHiddenInCurrentLanguage()
+    {
+        $currentLocale = $this->scopeConfig->getValue('general/locale/code', ScopeInterface::SCOPE_STORE);
+        $hiddenLanguages = $this->scopeConfig->getValue('payment/aplazame_payment/aplazame_widget/hide_in_languages', ScopeInterface::SCOPE_STORE);
+
+        if (in_array($currentLocale, explode(',', $hiddenLanguages))) {
+            return true;
+        }
+
+        return false;
     }
 }
